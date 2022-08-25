536ea830310da44ab3abe482ab79d6d7c8ee7e16
xiaodaigua <wang2896570008@163.com>
2022-08-25 14:36:15 +0800

    hod-fix first commit

--- src/main/java/com/pdsu/git/GitTist.java
+++ src/main/java/com/pdsu/git/GitTist.java
@@ -5,5 +5,6 @@ public class GitTist {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("222");
+        System.out.println("333333");
     }
 }
